492311bb193a4361321f2cde3ca65468ca274d95
Parth Sanjay Mahadik <parthsmahadik12027@gmail.com>
2026-03-02 19:58:10 +0530

    removed unused files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // @ts-ignore
   allowedDevOrigins: ["10.69.188.52", "192.168.1.100"],
 };
 
